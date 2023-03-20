f04d3d021d93ea538c567493d8aa14ee8f9f99b3
Francojara23 <128407772+Francojara23@users.noreply.github.com>
2023-03-20 13:50:16 -0300

    Add files via upload

---
+++ ejemplo 1.py
@@ -0,0 +1,11 @@
+__author__ = 'a6'
+
+#Datos
+cc=int(input("Cantidad de enfermos en el país: "))
+cn=int(input("Cantidad de enfermos en la ciudad: "))
+
+#Procesos
+pc=cc*100/cn
+
+#Resultados
+print("Porcentaje de la ciudad sobre el total del país: ",pc,"%")
